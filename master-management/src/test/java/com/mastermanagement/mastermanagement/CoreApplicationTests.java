9cbf05d3a6acf9098395301f690ade270b7a99d3
sebart7 <s3rodriguez10@gmail.com>
2022-12-23 11:03:22 -0500

    Initial project

---
+++ master-management/src/test/java/com/mastermanagement/mastermanagement/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mastermanagement.mastermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
